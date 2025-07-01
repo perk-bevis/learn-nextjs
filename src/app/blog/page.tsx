b4d6be0ade57ccc855d5194d17249ec40c83d1f2
perk-bevis <doanlq2k4@gmail.com>
2025-07-01 16:22:15 +0700

    loanding ui

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,7 +7,12 @@ export const metadata: Metadata = {
     }
 }
 
-export default function Blog(){
+export default async function Blog(){
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("done")
+        },2000)
+    })
     return (
         <h1>My blog</h1>
     )
